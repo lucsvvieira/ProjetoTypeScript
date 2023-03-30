389080b791c0bb810cd68c7338acc1967343e871
Lucas Vieira <vieira07lucas@gmail.com>
2023-03-30 16:28:36 -0300

    Implementando a programação defensiva e finalizando o projeto

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,6 +1,6 @@
 export class Negociacao {
     constructor(
-        public readonly data: Date, 
+        private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
@@ -9,5 +9,10 @@ export class Negociacao {
 
     get volume(): number {
         return this.quantidade * this.valor;  
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
